925cd4576cd0593a3d7165326db05d496f322340
dtmkeng <phumai@odds.team>
2020-10-31 13:47:19 +0700

    :tada: tailwind + styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mx-3 my-4 flex flex-col w-1/4">
+        <label htmlFor="firstname">First Name</label>
+        <input type="text" className="border-2 rounded-lg p-1" name="firstname"/>
+        <label htmlFor="firstname">Last Name</label>
+        <input type="text" className="border-2 rounded-lg p-1" name="firstname"/>
+        <label htmlFor="firstname">Age</label>
+        <input type="text" className="border-2 rounded-lg p-1" name="firstname"/>
+        <Title/>
+        <Wrapper/>
+      </div>
     </div>
   );
 }
